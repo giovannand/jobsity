c5b5067fb2fc23e4e330b5740b2fb95fad4842ad
Giovanna Damasceno <giovanna.damasceno.avenuecode@tembici-consult.com>
2022-02-20 13:30:57 -0300

    Adding postgis extension

--- db/init.sql
+++ db/init.sql
@@ -5,14 +5,14 @@ CREATE DATABASE jobsity;
 GRANT ALL PRIVILEGES ON DATABASE jobsity TO postgres;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
+CREATE EXTENSION IF NOT EXISTS postgis;
 
 
 CREATE TABLE trips(
     id UUID DEFAULT uuid_generate_v4(),
     region varchar(150) NOT NULL ,
-    origin_coord text NOT NULL,
-    destination_coord text NOT NULL ,
+    origin_coord geometry NOT NULL,
+    destination_coord geometry NOT NULL ,
     trip_datetime timestamp NOT NULL ,
     datasource varchar(150) NOT NULL  
 );
