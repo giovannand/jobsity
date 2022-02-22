651cea25469872515584e24dc8cf6b0f5b950a89
Giovanna Damasceno <giovanna.damasceno.avenuecode@tembici-consult.com>
2022-02-22 20:46:24 -0300

    Ads MULTIPLIER parameter for control the volume of data produced by producer

--- producer/src/producer.py
+++ producer/src/producer.py
@@ -5,13 +5,15 @@ import time
 import requests
 
 STATUS = os.environ['STATUS']
+MULTIPLIER = int(os.environ['MULTIPLIER'])
 
 if STATUS=='Online':
-    df = pd.read_csv('data/trips.csv')
+    for n in range(MULTIPLIER):
+        df = pd.read_csv('data/trips.csv')
 
-    for i in df.index:
-        data = df.loc[i].to_json()
-        r = requests.post('http://api:8080/trips',data = data) 
+        for i in df.index:
+            data = df.loc[i].to_json()
+            r = requests.post('http://api:8080/trips',data = data) 
 
 
             
